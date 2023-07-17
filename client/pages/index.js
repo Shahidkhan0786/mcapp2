39aba2812f6b08b3c8436586bf8912e8c0838bcd
Shahid Khan <130751667+shahid-khan-codegenio@users.noreply.github.com>
2023-07-17 18:23:45 +0500

    add some fn

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,35 @@
-export default ()=>{
-    return <h1>Landing page</h1>
-}
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  console.log(" I am in a component ");
+  return (
+    <>
+      <h1>Landing page</h1>;
+      {/* {currentUser ? "welcome" : "you are not authorized!"} */}
+    </>
+  );
+};
+
+LandingPage.getInitialProps = async () => {
+  //   console.log("i am on a server!");
+  // let $url = "";
+  if (typeof window === "undefined") {
+    console.log("on Serverrrrr");
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticket.com",
+        },
+      }
+    );
+    return data;
+  } else {
+    console.log("on Brow");
+
+    let $url = "/api/users/cuser";
+    const { data } = await axios.get($url);
+    console.log("on browser", data);
+    return data;
+  }
+};
+export default LandingPage;
